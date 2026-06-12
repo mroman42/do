c07eb6f73b3eef00aac05cd863e0340eb6a7890c
Mario Román <mromang08@gmail.com>
2026-06-12 09:45:47 +0300

    pearl-jeffrey unbiased

--- example/pearljeffrey.rkt
+++ example/pearljeffrey.rkt
@@ -1,34 +1,38 @@
 #lang racket
 
 (require do/leftdo)
+(require do/notation/unbias/leftDo)
 (require do/monad/norm)
 
-;; This is the example using urns.
+;; This file shows, as an example, the difference between Pearl and Jeffrey's
+;; updates. Pearl associates to the left; Jeffrey associates to the right.
 
 (define prior
   (distribution
-   ['red 1/5] ['blue 1/5] ['green 1/5] ['yellow 2/5]))
+   ['(red) 1/5] ['(blue) 1/5] ['(green) 1/5] ['(yellow) 2/5]))
 
 (define/match (shade c)
-  [('red)     (uniform 'bright)]
-  [('blue)    (uniform 'dark)]
-  [('green)   (uniform 'dark)]
-  [('yellow)  (uniform 'bright)])
+  [('red)     (uniform '(bright))]
+  [('blue)    (uniform '(dark))]
+  [('green)   (uniform '(dark))]
+  [('yellow)  (uniform '(bright))])
 
 (define pearl
-  (lDo Norm
-       r <- (distribution ['dark 7/10] ['bright 3/10])
-       c <- prior
-       s <- (shade c)
-       '() <- (observe s r)
-       return c))
+  (do Norm
+       (r) <- (distribution ['(dark) 7/10] ['(bright) 3/10])
+       (c) <- prior
+       (s) <- (shade c)
+       () <- (observe s r)
+       return (c)))
 
 (define jeffrey
-  (rDo Norm
-       r <- (distribution ['dark 7/10] ['bright 3/10])
-       c <- prior
-       s <- (shade c)
-       '() <- (observe s r)
-       return c))
+  (do Norm
+      (r c s) <- (do Norm
+                     (r) <- (distribution ['(dark) 7/10] ['(bright) 3/10])
+                     (c) <- prior
+                     (s) <- (shade c)
+                     return (r c s))
+      () <- (observe s r)
+      return (c)))
 
 
