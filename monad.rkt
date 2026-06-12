f7c8e6eec08edf203a75d4e126116064892cd2af
Mario Román <mromang08@gmail.com>
2026-06-12 09:46:28 +0300

    clean monad

--- monad.rkt
+++ monad.rkt
@@ -2,23 +2,10 @@
 
 ;; monad.rkt
 
-;; This file provides the interface for monads (return, bind) and
-;; implementations for multiple basic monads (List, Identity, Maybe, ...).
-;; The distribution monad is implemented separately (subdistributions.rkt).
+;; This file provides the interface for monads (return, bind). The
+;; implementation of particular monads is separated.
 
 (struct monad (return bind map))
-
-
-;; Continuation monad.
-;(define Cont
- ; (monad 
-   ;; return : x -> (x -> r) -> r
-  ; (λ (x) (λ (f) (f x)))
-   ;; bind : ((x -> r) -> r) ->
-   ;;        (x -> ((y -> r) -> r)) ->
-   ;;        ((y -> r) -> r)
-  ; (λ (d f) (λ (k) (d (λ (x) ((f x) k)))))
-  ; ))
 
 (define (monad-join M)
   (lambda (mmx)
